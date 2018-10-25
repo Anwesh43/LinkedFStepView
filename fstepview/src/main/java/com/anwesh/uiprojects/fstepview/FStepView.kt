09d0c49df5956ba28f2c6a3660a7f89145f2fe5d
Anwesh43 <anweshthecool0@gmail.com>
2018-10-25 09:13:58 +0530

    adding libraries

--- fstepview/src/main/java/com/anwesh/uiprojects/fstepview/FStepView.kt
+++ fstepview/src/main/java/com/anwesh/uiprojects/fstepview/FStepView.kt
@@ -4,3 +4,11 @@ package com.anwesh.uiprojects.fstepview
  * Created by anweshmishra on 25/10/18.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
