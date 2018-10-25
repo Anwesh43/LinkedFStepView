b0e3e70639c7df61d8d696dea79d3db5bf21d6f2
Anwesh43 <anweshthecool0@gmail.com>
2018-10-25 09:24:34 +0530

    adding FStepView class

--- fstepview/src/main/java/com/anwesh/uiprojects/fstepview/FStepView.kt
+++ fstepview/src/main/java/com/anwesh/uiprojects/fstepview/FStepView.kt
@@ -28,3 +28,21 @@ fun Canvas.drawFSNode(i : Int, scale : Float, paint : Paint) {
         drawLine(0f, y, (size/2) * (j + 1) * sc, y, paint)
     }
 }
+
+class FStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
