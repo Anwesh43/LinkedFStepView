1f57c1dc88970accd46f3bb8e533e54793f4318a
Anwesh43 <anweshthecool0@gmail.com>
2018-10-25 09:22:44 +0530

    adding drawFSNode canvas extension method

--- fstepview/src/main/java/com/anwesh/uiprojects/fstepview/FStepView.kt
+++ fstepview/src/main/java/com/anwesh/uiprojects/fstepview/FStepView.kt
@@ -12,3 +12,19 @@ import android.graphics.Color
 import android.content.Context
 
 val nodes : Int = 5
+
+fun Canvas.drawFSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / 3
+    paint.color = Color.parseColor("#673AB7")
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    drawLine(0f, -size, 0f, size, paint)
+    for (j in 0..1) {
+        val sc : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f * j)) * 2
+        val y : Float = -size * j
+        drawLine(0f, y, (size/2) * (j + 1) * sc, y, paint)
+    }
+}
